b6fe7a60f20145d32890ac8332b5d4b46a182058
Anderson Vieira <asvieira.br@gmail.com>
2022-12-27 14:26:37 -0500

    feat: header fixed on scrollY

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
 const buttonOpenMenu = document.querySelector('.nav-open-btn');
 const buttonCloseMenu = document.querySelector('.nav-close-btn');
 
-const navbar = document.querySelector('.navbar');
-const overlay = document.querySelector('.overlay');
-
 buttonOpenMenu.addEventListener('click', () => {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
@@ -15,3 +12,23 @@ buttonCloseMenu.addEventListener('click', () => {
   overlay.classList.remove("active")
   document.body.classList.remove("nav-active")
 })
+
+const navbar = document.querySelector('.navbar');
+const overlay = document.querySelector('.overlay');
+
+navbar.addEventListener('click', () => {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active")
+  document.body.classList.remove("nav-active")
+})
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active")
+  }
+})
+
