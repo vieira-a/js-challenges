24ddd3da7ff8353c6fd2f5718a4fa9421991e646
Anderson Vieira <asvieira.br@gmail.com>
2022-12-27 15:01:48 -0500

    feat: cleanup js code

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,22 @@
-const buttonOpenMenu = document.querySelector('.nav-open-btn');
-const buttonCloseMenu = document.querySelector('.nav-close-btn');
+'use strict';
 
-buttonOpenMenu.addEventListener('click', () => {
+function addEventOnElements(elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+const navbar = document.querySelector("[data-navbar]");
+const navTooglers = document.querySelectorAll("[data-nav-toogler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toogleNavbar = function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
-  document.body.classList.toggle("nav-active")
-})
+  document.body.classList.toggle("nav-active");
+}
 
-buttonCloseMenu.addEventListener('click', () => {
-  navbar.classList.remove("active");
-  overlay.classList.remove("active")
-  document.body.classList.remove("nav-active")
-})
-
-const navbar = document.querySelector('.navbar');
-const overlay = document.querySelector('.overlay');
-
-navbar.addEventListener('click', () => {
-  navbar.classList.remove("active");
-  overlay.classList.remove("active")
-  document.body.classList.remove("nav-active")
-})
+addEventOnElements(navTooglers, 'click', toogleNavbar)
 
 const header = document.querySelector("[data-header]");
 
@@ -28,7 +24,6 @@ window.addEventListener("scroll", () => {
   if(window.scrollY > 100) {
     header.classList.add("active");
   } else {
-    header.classList.remove("active")
+    header.classList.remove("active");
   }
 })
-
