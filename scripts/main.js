82e70573afc3a02d4fb36ff543af06c0b8af51d5
Anderson Vieira <asvieira.br@gmail.com>
2022-12-27 11:47:40 -0500

    fix: menu mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
-const buttonToogleMenu = document.getElementById('toogle-menu');
-const menuPage = document.querySelector('.menu')
-const menuLine1 = document.querySelector('.line1')
-const menuLine2 = document.querySelector('.line2')
-const menuLine3 = document.querySelector('.line3')
-console.log(menuLine1)
+const buttonOpenMenu = document.querySelector('.nav-open-btn');
+const buttonCloseMenu = document.querySelector('.nav-close-btn');
 
-buttonToogleMenu.addEventListener('click', function(){
-  if(!menuPage.classList.contains('active')){
-    menuPage.classList.replace('menu', 'active');
-    buttonToogleMenu.classList.add('button-active')
-  } else {
-    menuPage.classList.replace('active', 'menu')
-    buttonToogleMenu.classList.remove('button-active')
-  } 
+const navbar = document.querySelector('.navbar');
+const overlay = document.querySelector('.overlay');
+
+buttonOpenMenu.addEventListener('click', () => {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active")
+})
+
+buttonCloseMenu.addEventListener('click', () => {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active")
+  document.body.classList.remove("nav-active")
 })
