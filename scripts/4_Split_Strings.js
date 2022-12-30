1b0589b700755f60113f57b16531a4b67dac9f1a
Anderson Vieira <asvieira.br@gmail.com>
2022-12-29 23:59:14 -0300

    feat: challenge 3 - split into pair of two

--- scripts/4_Split_Strings.js
+++ scripts/4_Split_Strings.js
@@ -7,3 +7,36 @@
  * 'abcdef' => ['ab', 'cd', 'ef']
  */
 
+/**
+ * [x] Take intial position of the string character
+ * [x] Split the string into pair of two characters
+ * [ ] Aplly regex to validate typeof
+ */
+
+const myString = 'anderson';
+
+function validate(str) {
+
+    for(let pos1 = 0, pos2 = 1; pos2 <= str.length; pos1+=2, pos2+=2){
+
+        console.log(
+            (str[pos1] === undefined || typeof(pos1) === Number ? '_' : str[pos1]) +
+            (str[pos2] === undefined || typeof(pos2) === Number ? '_' : str[pos2]))
+        
+    }   
+}
+
+validate(myString)
+
+
+/* for(let i = 0; i < str.length; i++){
+    console.log(str.split('')).join(str[i+1])
+} */
+
+/* 
+
+const strSplited = str.split('');
+
+strSplited.forEach((e, i) => {
+    console.log(i[e])
+})  */
