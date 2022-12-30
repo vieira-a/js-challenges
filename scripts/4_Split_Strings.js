9e3529c867f65c522c4aeabf4ba138558c9d578d
Anderson Vieira <asvieira.br@gmail.com>
2022-12-29 22:32:45 -0300

    init challenge 4 - split strings

---
+++ scripts/4_Split_Strings.js
@@ -0,0 +1,9 @@
+/**
+ * Split the string into pair os two characters.
+ * If the strings contains an odd of characters then 
+ * it should replace the missing second character of the final pair with a underscore('_')
+ * Eg:
+ * 'abc' =>  ['ab', 'c_']
+ * 'abcdef' => ['ab', 'cd', 'ef']
+ */
+
