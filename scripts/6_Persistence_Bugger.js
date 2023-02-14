ec341e0cb0071c5dfc697a7954226b3dc0585d16
Anderson Vieira <asvieira.br@gmail.com>
2023-02-13 21:22:59 -0300

    feat: update menu with new challenges

--- scripts/6_Persistence_Bugger.js
+++ scripts/6_Persistence_Bugger.js
@@ -4,4 +4,10 @@
  * 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
  * 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
  * 4 --> 0 (because 4 is already a one-digit number)
+ * 
+ * Receber numero
+ * Separar digitos
+ * Verificar quantidade de digitos
+ * Enquanto a quantidade de digitos seja maior que 1:
+ * Fazer calculo entre o primeiro digito e os digitos subsequentes
  */
